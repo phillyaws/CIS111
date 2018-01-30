038471c7122bec990ff491a6ea54aa89963c334d
phillyaws <alexandria.w.schneider@gmail.com>
2018-01-29 22:04:46 -0500

    First homework

---
+++ TaxCalculator.java
@@ -0,0 +1,14 @@
+public class TaxCalculator {
+   public static main( String[] args )
+
+   double tax = 0.06;
+
+   double price;
+   double tax;
+   double total;
+
+      System.out.println(" the price of a " + price + " item after tax is " + total);
+      System.out.println(" the price of a " + price + " item after tax is " + total);
+      System.out.println(" the price of a " + price + " item after tax is " + total);
+
+}
