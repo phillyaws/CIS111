596cab3b001695032d5fdfa3e77efe372daea1cb
phillyaws <alexandria.w.schneider@gmail.com>
2018-01-31 20:05:27 -0500

    Shapes (2/7/18 homework)
    Some math with math.class jiggery.

---
+++ Shapes.java
@@ -0,0 +1,22 @@
+public class Shapes {
+   public static void main( String[] args ) {
+   
+   double r = 5; // Radius
+   double l = 10; // Length
+   double w = 20; // Width
+   
+   double cp = Math.PI * Math.pow(r, 2);
+   double ca = 2 * Math.PI * r ;
+   double rp = l + w + w + l;
+   double ra = l * w;
+   double rth = Math.pow(l, 2) + Math.pow(w, 2);
+   
+   System.out.println(" the perimeter of the circle is " + cp);
+   System.out.println(" The area of the circle is  " + ca);
+   System.out.println(" the perimeter of the rectangle is " + rp);
+   System.out.println(" The area of the rectangle is " + ra);
+   System.out.println(" The hypotenuse of the right triangle is " + rth);
+
+   
+   }
+}
