4eff3beced950c4e9bb20ad14940d6c69043f17a
phillyaws <alexandria.w.schneider@gmail.com>
2018-03-11 11:04:44 +0900

    Runners.java (3/7/18)

---
+++ Runners.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Runners {
+   public static void main( String[] args ) {
+   Scanner sc = new Scanner (System.in);
+   
+   // Runner 1
+   System.out.println("Tell me a runners name.  ");
+   String n1 = sc.next();
+   System.out.println("What was their time?");
+   double t1 = sc.nextDouble(); 
+   
+   //Runner 2
+   System.out.println("Tell me a runners name.  ");
+   String n2 = sc.next();
+   System.out.println("What was their time?");
+   double t2 = sc.nextDouble(); 
+   
+   // Runner 3
+   System.out.println("Tell me a runners name.  ");
+   String n3 = sc.next();
+   System.out.println("What was their time?");
+   double t3 = sc.nextDouble(); 
+   
+   r1 = n1 + t1;
+   r2 = n2 + t2;
+   r3 = n3 + t3;
+   
+   
+   
+;;
+
+   
