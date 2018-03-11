d982c68a335a2bebab924f5724eae4f86d5c7e46
phillyaws <alexandria.w.schneider@gmail.com>
2018-03-11 11:06:22 +0900

    Elementals.java 3/7/18

---
+++ Elementals.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Elementals {
+   public static void main( String[] args ) {
+   Scanner sc = new Scanner (System.in);
+   System.out.println("Pssst...wanna see a really nerdy magic trick?");
+   System.out.println("Give me a number for a circle's radius, any whole number.  ");
+   double r = sc.nextDouble(); // Radius
+   
+   System.out.println("Now give me another number.  This is going to be the length of a rectangle.  ");
+   double l = sc.nextDouble(); // Length
+   
+   System.out.println("Now give me one final number.  This is the WIDTH of the rectangle.  ");
+   double w = sc.nextDouble(); // Width
