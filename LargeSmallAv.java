18870dab8738af4b39de921fdf4c798cb14c9bff
phillyaws <alexandria.w.schneider@gmail.com>
2018-03-28 20:58:25 -0400

    Update LargeSmallAv.java

--- LargeSmallAv.java
+++ LargeSmallAv.java
@@ -9,22 +9,24 @@ import java.util.stream.*;
 public class LargeSmallAv {
    public static void main( String[] args ) 
    {
-   Scanner sc = new Scanner (System.in);
-   List<Integer> numbers = new ArrayList<Integer>();
-   System.out.print("Enter any series of numbers. Enter -99 as the last number.  ");
-   int num = sc.nextInt();
-   while ((num = sc.nextInt()) != -99) {
-      numbers.add(sc.nextInt());
-
-          }
+      Scanner sc = new Scanner (System.in);
+      List<Integer> numbers = new ArrayList<Integer>();
+      System.out.println("Enter any series of numbers. Enter -99 as the last number.  ");
+      int num = sc.nextInt();
+      while ((num = sc.nextInt()) != -99) {
+         int i = sc.nextInt();
+         numbers.add(i);
+      
+      }
       Integer number[]=numbers.toArray(new Integer[numbers.size()]);
- 
-          Collections.sort(numbers);
-          int total = numbers.size();
+   
+      Collections.sort(numbers);
+      int total = numbers.size();
+      Arrays.toString(number);
           
-          System.out.print(number);
-          System.out.print("The highest number you entered was: " + Collections.max(numbers));
-          System.out.print("The lowest number you entered was: " + Collections.min(numbers));
+      System.out.println(number.toString());
+      System.out.println("The highest number you entered was: " + Collections.max(numbers));
+      System.out.println("The lowest number you entered was: " + Collections.min(numbers));
           // code block end
    }
-   }
+}
