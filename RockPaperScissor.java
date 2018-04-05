7c3b21a8f754d36f0a0f0aad8b5abfcd73434528
phillyaws <alexandria.w.schneider@gmail.com>
2018-04-04 21:12:04 -0400

    Create RockPaperScissor.java

---
+++ RockPaperScissor.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class RockPaperScissor {
+   public static void main( String[] args ) {
+      Random r = new Random();
+      
+      int cc = r.nextInt(3);
+      
+      
+            if ( d1 > d2) {
+            System.out.println("The computer won with " + d1 + ".  ");
+            computer++;
