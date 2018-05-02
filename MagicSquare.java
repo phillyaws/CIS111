5a88a0aac1a660ea436d1ca3bd68cb9ab3ff54ed
phillyaws <alexandria.w.schneider@gmail.com>
2018-05-02 18:31:59 -0400

    Create MagicSquare.java

---
+++ MagicSquare.java
@@ -0,0 +1,23 @@
+// Program 19 on page 494 of the textbook: write a program that generates a magic square
+// Write a program that reads a list of words from an external test file, puts them into an array, and then prints them to the console.
+
+import java.util.*;
+
+
+public class MagicSquare {
+   public static void main( String[] args ) {
+   
+      public int loShu (int x, int y, int z)
+      {
+       System.out.println("Please enter a number between 1 and 9: ");
+         int[][] array = new int[3][3];
+      
+      }
+   
+      for (int i = 0; i < 10; i++) 
+      {
+         array[i] = rngesus.nextInt(9)+1;
+      }
+   
+   }
+}
